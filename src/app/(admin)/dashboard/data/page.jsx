cfba258daeb61e35f0dc9cf6c44a027739f5b417
Tang Newyen <codingwithout1and0s@gmail.com>
2025-06-08 18:14:12 -0500

    Comminting missing file

--- src/app/(admin)/dashboard/data/page.jsx
+++ src/app/(admin)/dashboard/data/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "git satston";
 import { Card, CardContent } from "@/components/ui/card";
 import useSurveyStore from "@/app/store/surveyStore";
 
